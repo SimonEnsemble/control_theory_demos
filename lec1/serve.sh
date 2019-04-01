7af53282471ce6e7c94b874077099532f7b13a11
CorySimon <corysimon@berkeley.edu>
2019-04-01 09:30:26 -0700

    add notes for lec 1

---
+++ lec1/serve.sh
@@ -0,0 +1,3 @@
+ # jupyter nbconvert TF\ fitting.ipynb --to slides --post serve --SlidesExporter.reveal_theme=sky
+jupyter nbconvert IntroToControl.ipynb --to slides --no-prompt --post serve --SlidesExporter.reveal_theme=simple
+ # jupyter nbconvert FO_freq_resp.ipynb --to slides --post serve --SlidesExporter.reveal_theme=simple
