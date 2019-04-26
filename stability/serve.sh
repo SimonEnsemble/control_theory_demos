f1ff4c94cf52e24fa96421da58709df71ee546e9
CorySimon <CorySimon@berkeley.edu>
2019-04-25 21:47:35 -0700

    add stability NB

---
+++ stability/serve.sh
@@ -0,0 +1,3 @@
+ # jupyter nbconvert TF\ fitting.ipynb --to slides --post serve --SlidesExporter.reveal_theme=sky
+jupyter nbconvert Stability\ of\ control\ systems.ipynb --to slides --no-prompt --post serve --SlidesExporter.reveal_theme=simple
+ # jupyter nbconvert FO_freq_resp.ipynb --to slides --post serve --SlidesExporter.reveal_theme=simple
